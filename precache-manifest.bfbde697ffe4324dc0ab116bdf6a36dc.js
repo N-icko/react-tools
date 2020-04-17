9cea1d19098b5f0d1c8e9086404c0501dc0d6c0a
Nickolay Kuzmyn <kuzmin.study@gmail.com>
2020-04-17 13:24:52 +0300

    Updates

---
+++ precache-manifest.bfbde697ffe4324dc0ab116bdf6a36dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742c09c81bbd5c2816a85360f52e0bdd",
+    "url": "/react-tools/index.html"
+  },
+  {
+    "revision": "1bb6e0383c8ec95a35d2",
+    "url": "/react-tools/static/css/main.e4b28931.chunk.css"
+  },
+  {
+    "revision": "fbc90d8a64ea53387f1e",
+    "url": "/react-tools/static/js/2.ecf3335c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-tools/static/js/2.ecf3335c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb6e0383c8ec95a35d2",
+    "url": "/react-tools/static/js/main.df856cdf.chunk.js"
+  },
+  {
+    "revision": "58f4e97481de8e775408",
+    "url": "/react-tools/static/js/runtime-main.40d1194a.js"
+  }
+]);
